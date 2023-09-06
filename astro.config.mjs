2daa4fbf06b5c7f45559aaa122854e4fddb1bdfd
Glenn Weatherson <glenn@970design.com>
2023-09-05 21:16:06 -0700

    update text page layout and combine styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,4 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://glennscrap.com',
 	integrations: [mdx(), sitemap()],
-  output: 'static',
-  adapter: vercelStatic(),
 });
