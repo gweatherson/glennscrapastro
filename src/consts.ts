e52b223ce710b2c7666828ee26bcabcfc97f9e5d
Glenn Weatherson <glenn@970design.com>
2023-09-08 22:19:39 -0700

    add ga

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Glenn's Crap";
-export const SITE_DESCRIPTION = '';
+export const SITE_DESCRIPTION = 'A site filled with photos (crap) from Glenn.';
